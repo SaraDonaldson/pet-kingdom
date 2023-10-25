dec30b6e41581ef8cee79b11eb32fce8cb361751
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-10-25 16:55:20 +0100

    write ssa to add new category details to prisma db

--- pet-kingdom/lib/functions/createCategory.ts
+++ pet-kingdom/lib/functions/createCategory.ts
@@ -5,20 +5,45 @@ import { Category } from "../types/Category";
 
 
 const prisma = new PrismaClient();
+// newCategory:Category
 
 async function createCategory(newCategory:Category) {
 
     
     try {
-        const name: Category = newCategory
+        
+    //  check duplicates with existing categories - name + parent = same 
+    //  return duplicate category name at parent message
+    //  check duplicates with existing order at level - order number + parent = same
+    //  return duplicate category order with parent
+  
+    // only for front end test of category
+    if (newCategory.parentId === ""){
+        newCategory.parentId = undefined
+    }
+    if (newCategory.headerImg === ""){
+        newCategory.headerImg = undefined
+    }
+    if (newCategory.description=== ""){
+        newCategory.description = undefined
+    }
+
+
+
+        const {name, link, order, parentId, headerImg, description}: Category = newCategory;
         const category = await prisma.category.create({
           data: {
             name:  name,
+            link: link,
+            order: order,
+            parentId:  parentId,
+            headerImg:  headerImg, 
+            description: description,
             },
           })
 
         
-          return newCategory
+          return category
       } catch (error:any) {
         return { error: error };
       }

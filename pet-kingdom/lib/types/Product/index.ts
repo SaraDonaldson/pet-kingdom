d20ff96d46ffc80f4e650bf1cacacf52db2d2c0b
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-10-25 17:07:25 +0100

    create product interface for prisma db columns

---
+++ pet-kingdom/lib/types/Product/index.ts
@@ -0,0 +1,11 @@
+export interface Product{
+    name: string,
+    categoryId: string|undefined, 
+    description:string|undefined,
+    inventory: number|undefined,
+    inventoryWarning: number|undefined,
+    price:number|undefined,
+     }
+
+
+   

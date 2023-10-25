ee66e47090dbb4f5e8479cf8bf503d386cee65f1
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-10-25 17:38:02 +0100

    update product intf to include all product cols

--- pet-kingdom/lib/types/Product/index.ts
+++ pet-kingdom/lib/types/Product/index.ts
@@ -1,10 +1,11 @@
 export interface Product{
     name: string,
-    categoryId: string|undefined, 
+    categoryId: string, 
     description:string|undefined,
     inventory: number|undefined,
     inventoryWarning: number|undefined,
     price:number|undefined,
+    imageUrl: string,
      }
 
 

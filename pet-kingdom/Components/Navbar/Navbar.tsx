6283d2e69e58a264561fbbe7a67b46471846bc48
CianMW <88888516+CianMW@users.noreply.github.com>
2024-01-10 18:08:52 +0000

    Add css module for Navbar, modify Navbar functionality

--- pet-kingdom/Components/Navbar/Navbar.tsx
+++ pet-kingdom/Components/Navbar/Navbar.tsx
@@ -1,79 +1,96 @@
-'use client'
+"use client";
 // import { getCookie } from '@/lib/functions/cookies/getCookie';
 // import { removeCookie } from '@/lib/functions/cookies/removeCookie';
-import { cookies } from 'next/headers';
-import Image from 'next/image';
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-const Navbar: React.FC = () => {
-const router = useRouter()
-const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
-const [isAdmin, setIsAdmin] = useState<boolean>(false)
-const pathname = usePathname()
+import React from "react";
+import styles from "./Navbar.module.css";
+import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 
-function logOut() {
-  if (typeof window !== "undefined") {
-    // removeCookie('loggedIn');
-    localStorage.clear();
-    setIsLoggedIn(false);
-    if(pathname === '/'){
+function Navbar() {
+const router = useRouter();
+const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+const [isAdmin, setIsAdmin] = useState<boolean>(false);
+const pathname = usePathname();
 
-      router.refresh();
-    } else {
-      router.push('/')
+  function logOut() {
+    if (typeof window !== "undefined") {
+      // removeCookie('loggedIn');
+      localStorage.clear();
+      setIsLoggedIn(false);
+      if (pathname === "/") {
+        router.refresh();
+      } else {
+        router.push("/");
+      }
     }
   }
-}
 
-async function checkLoggedIn() {
-  console.log('trying')
-  // if (typeof window !== "undefined") {
-    const foundUser = localStorage.getItem('user');
+  async function checkLoggedIn() {
+    console.log("trying");
+    // if (typeof window !== "undefined") {
+    const foundUser = localStorage.getItem("user");
     if (foundUser) {
       setIsLoggedIn(true);
       const parsedUser = await JSON.parse(foundUser);
-      if (parsedUser.role !== 'user') {
+      if (parsedUser.role !== "user") {
         setIsAdmin(true);
         // setTimeoutActive(false);
       }
     }
-  // } else {
-  //   setTimeout(checkLoggedIn, 300)
-  // }
-}
+    // } else {
+    //   setTimeout(checkLoggedIn, 300)
+    // }
+  }
 
+  useEffect(() => {
+    //@ts-ignore
+    window.addEventListener("storage", checkLoggedIn());
+    //@ts-ignore
+    return () => window.removeEventListener("storage", checkLoggedIn());
+  }, []);
 
-useEffect(() => {
-//@ts-ignore
-window.addEventListener('storage', checkLoggedIn())
-//@ts-ignore
-    return () => window.removeEventListener('storage', checkLoggedIn())
-}, []);
-  
   return (
-    <div className="bg-gradient-to-r via-southPeach via-70% to-transparent  from-coral bg-opacity-45 absolute top-0 left-0 w-full px-4 flex items-center justify-between">
+    <nav className={styles.navbar}>
       {/* Logo */}
-      <div onClick={()=> router.push('/')}className="flex cursor-pointer items-center">
-      Pet Kingdom
-      </div>
-      {/* Buttons */}
-    {
-      isLoggedIn && (
-      <div className=" space-x-4">
-        {isAdmin && 
-        <button onClick={() => router.push(pathname.includes('admin') ? '/':'admin')} className="border-2 border-white bg-southPeach hover:shadow-md transition-all duration-300 px-4 py-2 rounded  hover:bg-opacity-80">
-          {pathname.includes('admin') ? 'User':'Admin'} View
-        </button>
-        }
-        <button onClick={logOut} className="border-2 border-white bg-southPeach hover:shadow-md transition-all px-4 py-2 rounded  hover:bg-opacity-80">
-          Log Out
-        </button>
-      </div>
-        )
-      }
-    </div>
+      <div className={styles.logo}>LOGO</div>
+      <div className={styles.searchBar}>
+        <input type="text" placeholder="Search..." />
+        <AiOutlineSearch size="1.5em" />
+        </div>
+        {/* Buttons */}
+        {isLoggedIn && (
+          <div className=" space-x-4">
+            {isAdmin && (
+              <button
+                onClick={() =>
+                  router.push(pathname.includes("admin") ? "/" : "admin")
+                }
+                className="border-2 border-white bg-southPeach hover:shadow-md transition-all duration-300 px-4 py-2 rounded  hover:bg-opacity-80"
+              >
+                {pathname.includes("admin") ? "User" : "Admin"} View
+              </button>
+            )}
+            <button
+              onClick={logOut}
+              className="border-2 border-white bg-southPeach hover:shadow-md transition-all px-4 py-2 rounded  hover:bg-opacity-80"
+            >
+              Log Out
+            </button>
+          </div>
+        )}
+        <div className={styles.navItems}>
+          <CgProfile size="1.5em" className={styles.icon} />
+          <IoMdHeartEmpty size="1.5em" className={styles.icon} />
+          <AiOutlineShoppingCart size="1.5em" className={styles.icon} />
+        </div>
+    </nav>
   );
-};
 
+}
 export default Navbar;
--- pet-kingdom/app/layout.tsx
+++ pet-kingdom/app/layout.tsx
@@ -19,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       
+      <body className={inter.className}>
       <Navbar/>
-      <body className={inter.className}>{children}</body>
+        {children}
       <Footer />
+        </body>
     
     </html>
    

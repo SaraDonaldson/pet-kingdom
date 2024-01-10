3a5ce98b0438084c4a84a6b9b6374d0a68b7c7c0
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2024-01-10 16:20:32 +0000

    Update button

---
+++ pet-kingdom/Components/Common/Button/index.tsx
@@ -0,0 +1,12 @@
+
+
+
+interface Props{
+clickFunction:Function
+label:string
+}
+export default function Button ({clickFunction, label}:Props){
+        return (
+<div className={"w-24 text-lg font-semibold bg-purple-600 hover:bg-opacity-60 text-center "} onClick={() => clickFunction()}>{label}</div>
+        )
+}
